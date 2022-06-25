56764ebb6d8737aac3bc3b4891cbbf3c02b7d550
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-25 09:17:53 -0300

    update WorldChatEvent.php

--- src/WorldManager/Eventos/WorldManagerEvents/WorldChatEvent.php
+++ src/WorldManager/Eventos/WorldManagerEvents/WorldChatEvent.php
@@ -3,36 +3,39 @@
 namespace WorldManager\Eventos\WorldManagerEvents;
 
 use WorldManager\Loader;
-use pocketmine\utils\Config;   
+use pocketmine\utils\Config;
 
 class WorldChatEvent {
- 
+
  public function __construct($user, $world, $bool) {
-  $player = $user;
+  $this->execute($user, $world, $bool);
+ }
+
+ public function execute($player, $world, $bool) {
   $plugin = Loader::getInstance();
   $server = $plugin->getServer();
   $data = $plugin->getDataFolder();
   $config = new Config($data . "worlds.yml");
   $worlds = $config->getAll();
-  
-  if($bool == true){
-   if($worlds["$world"]["chat"] == true){
+
+  if ($bool == true) {
+   if ($worlds["$world"]["chat"] == true) {
     $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.chat.unlock.has")));
     return true;
    }
    $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.chat.unlock.sucess")));
   }
-  
-  if($bool == false){
-   if($worlds["$world"]["chat"] == false){
-   $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.chat.lock.has")));
+
+  if ($bool == false) {
+   if ($worlds["$world"]["chat"] == false) {
+    $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.chat.lock.has")));
     return true;
    }
    $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.chat.lock.sucess")));
   }
-  
-  $config->setNested("$world.chat", $bool); 
-  $config->save(); 
+
+  $config->setNested("$world.chat", $bool);
+  $config->save();
  }
- 
-}  
+
+}
