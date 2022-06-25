89599d9f130e9c3a76a5eaa4433a52fbb3161051
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-25 09:22:02 -0300

    update WorldUnlockEvent.php

--- src/WorldManager/Eventos/WorldManagerEvents/WorldUnlockEvent.php
+++ src/WorldManager/Eventos/WorldManagerEvents/WorldUnlockEvent.php
@@ -3,27 +3,30 @@
 namespace WorldManager\Eventos\WorldManagerEvents;
 
 use WorldManager\Loader;
-use pocketmine\utils\Config;  
+use pocketmine\utils\Config;
 
 class WorldUnlockEvent {
- 
+
  public function __construct($user, $world) {
-  $player = $user;
+  $this->execute($user, $world);
+ }
+
+ public function execute($player, $world) {
   $plugin = Loader::getInstance();
   $server = $plugin->getServer();
   $data = $plugin->getDataFolder();
   $world = $player->getWorld()->getDisplayName();
   $config = new Config($data . "worlds.yml");
-  $worlds = $config->getAll(); 
-  
-  if($worlds["$world"]["protected"] == false){
-   $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.unlock.has")));        
+  $worlds = $config->getAll();
+
+  if ($worlds["$world"]["protected"] == false) {
+   $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.unlock.has")));
    return true;
   }
-  
+
   $config->setNested("$world.protected", false);
-  $config->save(); 
-  $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.unlock.sucess")));       
+  $config->save();
+  $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.unlock.sucess")));
  }
- 
+
 }
