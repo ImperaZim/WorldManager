5430af5b25a720c5580a39c8d3f998526e2839ae
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-25 09:18:46 -0300

    update WorldCombatEvent.php

--- src/WorldManager/Eventos/WorldManagerEvents/WorldCombatEvent.php
+++ src/WorldManager/Eventos/WorldManagerEvents/WorldCombatEvent.php
@@ -3,36 +3,39 @@
 namespace WorldManager\Eventos\WorldManagerEvents;
 
 use WorldManager\Loader;
-use pocketmine\utils\Config;   
+use pocketmine\utils\Config;
 
 class WorldCombatEvent {
- 
+
  public function __construct($user, $world, $bool) {
-  $player = $user;
+  $this->execute($user, $world, $bool);
+ }
+
+ public function execute($player, $world, $bool) {
   $plugin = Loader::getInstance();
   $server = $plugin->getServer();
   $data = $plugin->getDataFolder();
   $config = new Config($data . "worlds.yml");
   $worlds = $config->getAll();
-  
-  if($bool == true){
-   if($worlds["$world"]["combat"] == true){
-    $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.combat.enable.has")));   
+
+  if ($bool == true) {
+   if ($worlds["$world"]["combat"] == true) {
+    $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.combat.enable.has")));
     return true;
    }
-   $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.combat.enable.sucess")));   
+   $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.combat.enable.sucess")));
   }
-  
-  if($bool == false){
-   if($worlds["$world"]["combat"] == false){
-    $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.combat.disable.has")));   
+
+  if ($bool == false) {
+   if ($worlds["$world"]["combat"] == false) {
+    $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.combat.disable.has")));
     return true;
    }
-   $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.combat.disable.sucess"))); 
+   $player->sendMessage($plugin->getProcessedTags($player, $world, $plugin->getConfig()->get("command.combat.disable.sucess")));
   }
-  
-  $config->setNested("$world.combat", $bool); 
-  $config->save(); 
+
+  $config->setNested("$world.combat", $bool);
+  $config->save();
  }
- 
-} 
+
+}
