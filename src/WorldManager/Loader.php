fcab281a98b725628ca197a22f5a8f52620a5de3
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-25 09:23:27 -0300

    update Loader.php

--- src/WorldManager/Loader.php
+++ src/WorldManager/Loader.php
@@ -11,7 +11,7 @@ use WorldManager\{
 use pocketmine\{
  Server, 
  utils\Config,
- Player\Player, 
+ player\Player, 
  plugin\PluginBase, 
  utils\TextFormat as C, 
 };
@@ -58,40 +58,5 @@ class Loader extends PluginBase {
   $message = str_replace($tags, $processed, $message);
   return $message;
  }
- 
- public function generateChunk($chunkX, $chunkZ)
- {
-     $this->random->setSeed(0xdeadbeef ^ $chunkX << 8 ^ $chunkZ ^ $this->level->getSeed());
-     $noise = Generator::getFastNoise3D($this->noiseBase, 16, 128, 16, 4, 8, 4, $chunkX * 16, 0, $chunkZ * 16);
-     $chunk = $this->level->getChunk($chunkX, $chunkZ);
-     for ($x = 0; $x < 16; ++$x) {
-         for ($z = 0; $z < 16; ++$z) {
-             $biome = Biome::getBiome(Biome::HELL);
-             $chunk->setBiomeId($x, $z, $biome->getId());
-             $color = [0, 0, 0];
-             $bColor = $biome->getColor();
-             $color[0] += ($bColor >> 16) ** 2;
-             $color[1] += ($bColor >> 8 & 0xff) ** 2;
-             $color[2] += ($bColor & 0xff) ** 2;
-             $chunk->setBiomeColor($x, $z, $color[0], $color[1], $color[2]);
-             for ($y = 0; $y < 128; ++$y) {
-                 if ($y === 0 or $y === 127) {
-                     $chunk->setBlockId($x, $y, $z, Block::BEDROCK);
-                     continue;
-                 }
-                 $noiseValue = abs($this->emptyHeight - $y) / $this->emptyHeight * $this->emptyAmplitude - $noise[$x][$z][$y];
-                 $noiseValue -= 1 - $this->density;
-                 if ($noiseValue > 0) {
-                     $chunk->setBlockId($x, $y, $z, Block::NETHERRACK);
-                 } elseif ($y <= $this->waterHeight) {
-                     $chunk->setBlockId($x, $y, $z, Block::STILL_LAVA);
-                 }
-             }
-         }
-     }
-     foreach ($this->generationPopulators as $populator) {
-         $populator->populate($this->level, $chunkX, $chunkZ, $this->random);
-     }
- } 
  
 }
